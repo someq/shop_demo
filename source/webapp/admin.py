9611dded8663d03fb9ed12404aee586b79aea7b5
Farid <thesomeq@gmail.com>
2019-11-07 21:20:24 +0600

    Добавлено оформление заказа.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from webapp.models import Product
+from webapp.models import Product, Order, OrderProduct
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -7,4 +7,17 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ('category',)
 
 
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    fields = ('product', 'amount')
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone', 'email', 'created_at')
+    list_filter = ('status',)
+    inlines = (OrderProductInline, )
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
