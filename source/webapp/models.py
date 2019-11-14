657a9a54579808f81d4b056b186b68a683794cb9
Farid <thesomeq@gmail.com>
2019-11-14 19:37:13 +0600

    Добавлено поле "В наличии" для товара со значением True по умолчанию.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
                                 verbose_name='Категория')
     photo = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Фото')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    in_order = models.BooleanField(verbose_name='В наличии', default=True)
 
     def __str__(self):
         return self.name
